8ace72eccf3dd72d5c67c163b066d9e61d925e87
Melvyn de Kort <melvyn@mdekort.nl>
2019-06-04 00:07:02 +0200

    Several bug fixes in Lambda NodeJS code

--- src/app.js
+++ src/app.js
@@ -7,26 +7,56 @@ const errorUnexpected = new Error('Unexpected error')
 const errorUnauthorized = new Error('Unauthorized')
 
 exports.lambdaHandler = async (event) => {
-    cookieDomain = getCookieDomain(event);
-    expiration = getExpirationFromToken(event);
-    cookies = await buildCookie(cookieDomain, expiration * 1000)
-    return JSON.stringify(cookies)
+    try {
+        cookieDomain = getCookieDomain(event);
+        expiration = getExpirationFromToken(event);
+        cookies = await buildCookie(cookieDomain, expiration * 1000);
+        return {
+            "statusCode": 200,
+            "headers": {
+                "Access-Control-Allow-Origin": cookieDomain,
+                "Access-Control-Allow-Methods": "GET, OPTIONS"
+            },
+            "body": JSON.stringify(cookies)
+        }
+    }
+    catch (err) {
+        console.log(err);
+        return {
+            "statusCode": 401,
+            "headers": {
+                "Access-Control-Allow-Origin": cookieDomain,
+                "Access-Control-Allow-Methods": "GET, OPTIONS"
+            },
+            "body": err.message
+        }
+    }
+}
+
+function keysToLowercase(obj) {
+    Object.fromEntries = arr => Object.assign({}, ...Array.from(arr, ([k, v]) => ({ [k]: v })));
+
+    entries = Object.entries(obj).map(([k, v]) => [k.toLowerCase(), v]);
+    result = Object.fromEntries(entries);
+    return result;
 }
 
 function getCookieDomain(event) {
-    if (!'origin' in event.headers) {
+    headers = keysToLowercase(event.headers);
+    if (!('origin' in headers)) {
         console.log('origin header missing');
         throw errorUnexpected;
     }
-    cookieDomain = event.headers['origin'];
+    return headers['origin'];
 }
 
 function getExpirationFromToken(event) {
-    if (!'id_token' in event.queryStringParameters) {
+    parameters = keysToLowercase(event.queryStringParameters);
+    if (!('id_token' in parameters)) {
         console.log('id_token paramter missing');
         throw errorUnauthorized;
     }
-    token = event.queryStringParameters['id_token'];
+    token = parameters['id_token'];
 
     try {
         decoded = jwt.verify(token, process.env.AUTH0_PUB_KEY);
