4770d5dd267d8840e21cbc02303ac369ff90ac8b
Melvyn de Kort <melvyn@mdekort.nl>
2022-05-09 04:00:55 +0200

    Terraform format

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -11,5 +11,6 @@ data "aws_iam_policy_document" "lambda_assume_role_policy" {
 
 resource "aws_iam_role" "iam_for_lambda" {
   name = "iam_for_lambda"
+
   assume_role_policy = data.aws_iam_policy_document.lambda_assume_role_policy.json
 }
