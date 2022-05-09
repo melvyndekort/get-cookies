413adc319f1c17a238a32e15cb8935f9427eaadf
Melvyn de Kort <melvyn@mdekort.nl>
2022-05-09 15:52:33 +0200

    Convert lambda to Python (#2)
    * Convert the lambda to python
    
    * Set location to download dependencies to
    
    * Set location to download dependencies to
    
    * Updated pipeline and changed lambda runtime
    
    * Also build on PRs
    
    * Fix permissions
    
    * Limit builds to only PRs and pushes to main

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -2,9 +2,9 @@ resource "aws_lambda_function" "convert-jwt" {
   filename      = "lambda.zip"
   function_name = "convert-jwt"
   role          = aws_iam_role.iam_for_lambda.arn
-  handler       = "app.lambdaHandler"
+  handler       = "lambda_function.lambda_handler"
 
   source_code_hash = filebase64sha256("lambda.zip")
 
-  runtime = "nodejs12.x"
+  runtime = "python3.9"
 }
