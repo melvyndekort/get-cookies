8301a9a9b150d85ed280e499922fd956c95c595b
Melvyn de Kort <melvyn@mdekort.nl>
2022-05-14 08:01:52 +0200

    Setup API gateway access logging

--- terraform/api_gateway.tf
+++ terraform/api_gateway.tf
@@ -1,6 +1,13 @@
 resource "aws_apigatewayv2_api" "auth" {
   name          = "AuthAPI"
   protocol_type = "HTTP"
+
+  cors_configuration {
+    allow_methods = ["GET", "OPTIONS"]
+    allow_origins = [
+      "https://example.melvyn.dev",
+    ]
+  }
 }
 
 resource "aws_apigatewayv2_integration" "auth" {
@@ -16,10 +23,34 @@ resource "aws_apigatewayv2_route" "auth" {
   target    = "integrations/${aws_apigatewayv2_integration.auth.id}"
 }
 
+resource "aws_cloudwatch_log_group" "auth" {
+  name              = "/aws/apigateway/AuthAPI"
+  retention_in_days = 14
+}
+
 resource "aws_apigatewayv2_stage" "auth" {
   api_id      = aws_apigatewayv2_api.auth.id
   name        = "$default"
   auto_deploy = true
+
+  access_log_settings {
+    destination_arn = aws_cloudwatch_log_group.auth.arn
+    format = jsonencode({
+  "requestId":               "$context.requestId",
+  "extendedRequestId":       "$context.extendedRequestId",
+  "ip":                      "$context.identity.sourceIp",
+  "caller":                  "$context.identity.caller",
+  "user":                    "$context.identity.user",
+  "requestTime":             "$context.requestTime",
+  "httpMethod":              "$context.httpMethod",
+  "resourcePath":            "$context.resourcePath",
+  "routeKey":                "$context.routeKey",
+  "status":                  "$context.status",
+  "protocol":                "$context.protocol",
+  "responseLength":          "$context.responseLength",
+  "integrationErrorMessage": "$context.integrationErrorMessage"
+})
+  }
 }
 
 resource "aws_apigatewayv2_domain_name" "api" {
