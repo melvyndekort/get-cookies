9251aa2e560bd123e6af76cd96a456ab224bfc5a
Melvyn de Kort <melvyn@mdekort.nl>
2022-05-18 14:22:36 +0200

    Python cleanup (#6)
    * Setup API gateway access logging
    
    * Better lambda code setup
    
    * Terraform format
    
    * Update private key
    
    * Use new private key
    
    * Lots of improvements
    
    * Terraform format

---
+++ src/signed_cookie_generator.py
@@ -0,0 +1,73 @@
+import time
+import json
+import base64
+import boto3
+import os
+
+from datetime import datetime
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives import serialization
+from cryptography.hazmat.primitives.asymmetric import padding
+
+class CookieGen:
+
+  def aws_base64_encode(self, data):
+    return base64.b64encode(data).replace(b'+', b'-').replace(b'=', b'_').replace(b'/', b'~')
+
+  def aws_base64_decode(self, data):
+    return base64.b64decode(data.replace(b'-', b'+').replace(b'_', b'=').replace(b'~', b'/'))
+
+  def rsa_signer(self, message):
+    ssm_client = boto3.client('ssm')
+
+    param = ssm_client.get_parameter(
+      Name=os.environ['CLOUDFRONT_PK_PATH'],
+      WithDecryption=True
+    )
+
+    private_key = serialization.load_pem_private_key(
+      data=param['Parameter']['Value'].encode(),
+      password=None,
+      backend=default_backend()
+    )
+
+    return private_key.sign(message, padding.PKCS1v15(), hashes.SHA1())
+
+  def make_policy(self, resource, expire_date):
+    policy = {
+      'Statement': [{
+        'Resource': resource,
+        'Condition': {
+          'DateLessThan': {
+            'AWS:EpochTime': expire_date
+          }
+        }
+      }]
+    }
+    return json.dumps(policy).replace(" ", "")
+
+  def generate_signed_cookie(self, key_id, policy, expire_date):
+    policy = policy.encode('utf8')
+    policy_b64 = self.aws_base64_encode(policy)
+
+    signature = self.rsa_signer(policy)
+    signature_b64 = self.aws_base64_encode(signature)
+
+    return {
+      'Policy'    : policy_b64.decode("utf-8"),
+      'Signature' : signature_b64.decode("utf-8"),
+      'Key'       : key_id,
+      'Expiration': expire_date
+    }
+
+  def epoch_offset_from_now(self, offset_sec):
+    now = datetime.now()
+    epoch_offset = int(time.mktime(now.timetuple()))
+    epoch_offset += offset_sec
+    return epoch_offset
+
+  def generate_expiring_signed_cookie(self, resource, expire_date, key_id):
+    policy = self.make_policy(resource, expire_date)
+    signed_cookie = self.generate_signed_cookie(key_id, policy, expire_date)
+    return signed_cookie
---
+++ terraform/cognito.tf
@@ -0,0 +1,19 @@
+resource "aws_cognito_user_pool_client" "get_cookies" {
+  name         = "get_cookies"
+  user_pool_id = data.terraform_remote_state.cloudsetup.outputs.sso_user_pool_id
+
+  allowed_oauth_flows_user_pool_client = true
+  allowed_oauth_flows                  = ["code", "implicit"]
+  allowed_oauth_scopes                 = ["email", "openid"]
+  supported_identity_providers         = ["COGNITO"]
+
+  explicit_auth_flows = [
+    "ALLOW_USER_PASSWORD_AUTH",
+    "ALLOW_REFRESH_TOKEN_AUTH"
+  ]
+
+  callback_urls = [
+    "https://example.melvyn.dev/callback.html",
+    "https://example.melvyn.dev"
+  ]
+}
