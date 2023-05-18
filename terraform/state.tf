07059db9c0e3335b973165873b580287b5567eb3
Melvyn de Kort <melvyn@mdekort.nl>
2023-05-18 09:40:53 +0200

    Use Terraform Cloud as remote state provider

--- terraform/state.tf
+++ terraform/state.tf
@@ -9,11 +9,12 @@ terraform {
 }
 
 data "terraform_remote_state" "cloudsetup" {
-  backend = "s3"
+  backend = "remote"
 
   config = {
-    bucket = "mdekort.tfstate"
-    key    = "cloudsetup.tfstate"
-    region = "eu-west-1"
+    organization = "melvyndekort"
+    workspaces   = {
+      name = "cloudsetup"
+    }
   }
 }
