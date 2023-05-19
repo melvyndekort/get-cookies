b2804aaba1653d73dbeefb715606349049ab1e66
Melvyn de Kort <melvyn@mdekort.nl>
2023-05-19 13:48:30 +0200

    Migrate state to Terraform Cloud

--- terraform/state.tf
+++ terraform/state.tf
@@ -1,13 +1,3 @@
-terraform {
-  backend "s3" {
-    bucket     = "mdekort.tfstate"
-    key        = "convert-jwt.tfstate"
-    region     = "eu-west-1"
-    encrypt    = "true"
-    kms_key_id = "arn:aws:kms:eu-west-1:075673041815:alias/generic"
-  }
-}
-
 data "terraform_remote_state" "cloudsetup" {
   backend = "remote"
 
