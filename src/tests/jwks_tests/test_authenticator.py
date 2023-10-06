0a8d8c553429a64bb6fa3bf0f289adee7af08aab
Melvyn de Kort <melvyn@mdekort.nl>
2023-10-06 17:27:42 +0200

    Moved fixtures to conftest.py

--- src/tests/jwks_tests/test_authenticator.py
+++ src/tests/jwks_tests/test_authenticator.py
@@ -11,37 +11,6 @@ from cryptography.hazmat.primitives.serialization import load_pem_public_key
 os.environ['JWKS_LIST'] = ''
 from get_cookies import authenticator
 
-##
-# Fixtures
-##
-@pytest.fixture
-def non_matching_jwks():
-  jwks = {
-    "keys": [
-      {
-        "kty": "RSA",
-        "n": "foo",
-        "e": "FOO",
-        "kid": "foo"
-      }
-    ]
-  }
-  return json.dumps(jwks)
-
-@pytest.fixture
-def matching_jwks():
-  jwks = {
-    "keys": [
-      {
-        "kty": "RSA",
-        "n": "u1SU1LfVLPHCozMxH2Mo4lgOEePzNm0tRgeLezV6ffAt0gunVTLw7onLRnrq0_IzW7yWR7QkrmBL7jTKEn5u-qKhbwKfBstIs-bMY2Zkp18gnTxKLxoS2tFczGkPLPgizskuemMghRniWaoLcyehkd3qqGElvW_VDL5AaWTg0nLVkjRo9z-40RQzuVaE8AkAFmxZzow3x-VJYKdjykkJ0iT9wCS0DRTXu269V264Vf_3jvredZiKRkgwlL9xNAwxXFg0x_XFw005UWVRIkdgcKWTjpBP2dPwVZ4WWC-9aGVd-Gyn1o0CLelf4rEjGoXbAAEgAqeGUxrcIlbjXfbcmw",
-        "e": "AQAB",
-        "kid": "correct"
-      }
-    ]
-  }
-  return json.dumps(jwks)
-
 #def test_get_public_key_success_single(monkeypatch, der, matching_jwks, valid_token):
 #  #requests_mock.get('http://matching', text=matching_jwks)
 #
