3110d1da34cedd4a1f153d9d45b2692d76a9a16d
Melvyn de Kort <melvyn@mdekort.nl>
2023-10-07 09:02:14 +0200

    Initialize the signer in tests

--- src/tests/test_handler.py
+++ src/tests/test_handler.py
@@ -1,11 +1,5 @@
-#import os
-#
-#requests_mock.get('http://non-matching', text=non_matching_jwks)
-#
-#os.environ['CLOUDFRONT_PK_PATH'] = ''
-#from get_cookies import handler
-#
-#requests_mock
-#
-#def test_handle_success(requests_mock):
-#  assert False
+import pytest
+
+def test_initialize(init_signer):
+  from get_cookies import handler
+  assert True
