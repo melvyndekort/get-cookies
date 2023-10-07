1faa6446b927b78ed97a9634463e9684d31373c2
Melvyn de Kort <melvyn@mdekort.nl>
2023-10-07 09:02:14 +0200

    Initialize the signer in tests

--- src/get_cookies/signer.py
+++ src/get_cookies/signer.py
@@ -7,7 +7,9 @@ import requests
 
 from datetime import datetime
 
+from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives import serialization
 from cryptography.hazmat.primitives.asymmetric import padding
 
 logger = logging.getLogger()
---
+++ src/tests/test_signer.py
@@ -0,0 +1,5 @@
+import pytest
+
+def test_sign(init_signer):
+  from get_cookies import signer
+  assert True
