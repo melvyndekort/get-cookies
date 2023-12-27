1f92f996737cbe8d56b2f21884d2871d4b5ac24a
Melvyn de Kort <melvyn@mdekort.nl>
2023-12-27 15:46:45 +0100

    More precise logging

--- get_cookies/converter.py
+++ get_cookies/converter.py
@@ -17,7 +17,7 @@ def get_cookies(token, origin):
 
     time_ms = round(time.time() * 1000)
     if expire_date < time_ms + (1000 * 60 * 60):
-        logger.info('Token is short-lived, setting expiration to a week')
         expire_date = time_ms + (1000 * 60 * 60 * 24 * 7)
+        logger.info('Token is short-lived, setting expiration to 7 days: %s', expire_date)
 
     return signer.generate_expiring_signed_cookie(resource=resource, expire_date=expire_date)
