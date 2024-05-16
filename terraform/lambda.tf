8a25bf4a77a722d6424621ceeaa9e6de7082267d
Melvyn de Kort <melvyn@mdekort.nl>
2024-05-16 15:27:19 +0200

    Remove X-Ray config, since we're now using New Relic

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -34,10 +34,6 @@ resource "aws_lambda_function" "get_cookies" {
   memory_size   = 128
   timeout       = 8
 
-  tracing_config {
-    mode = "Active"
-  }
-
   kms_key_arn = data.aws_kms_key.generic.arn
 
   environment {
