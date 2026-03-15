ff3767891cb783e66eacb1273ef614172a3f2426
Melvyn de Kort <melvyn@mdekort.nl>
2026-03-15 11:42:32 +0100

    Migrate state to consolidated tfstate bucket

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -2,7 +2,7 @@ terraform {
   required_version = "~> 1.10"
 
   backend "s3" {
-    bucket       = "mdekort.tfstate"
+    bucket       = "mdekort-tfstate-075673041815"
     key          = "get-cookies.tfstate"
     region       = "eu-west-1"
     encrypt      = true
